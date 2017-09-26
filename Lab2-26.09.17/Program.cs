42b846ad8d920aabdf16cecc6d19b2e0ca802670
Ishhh <Ishhh@DESKTOP-NCDQNMO>
2017-09-26 09:47:38 +0200

    Add project files.

---
+++ Lab2-26.09.17/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2_26._09._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
